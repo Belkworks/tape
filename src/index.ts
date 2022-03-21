c565799875d1419323ae009dbce14e8abef786a3
safazi <safazi@airmail.cc>
2022-03-20 22:24:34 -0500

    opts and readme

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,8 @@ program
 	.option('-d --dir <dir>', 'target directory', 'src')
 	.option('-v --verbose', 'enable verbose output', false)
 	.option('--main <name>', 'lua entrypoint', 'init')
-	.option('-o --output <path>', 'output file', 'dist.lua');
+	.option('-o --output <path>', 'output file', 'dist.lua')
+	.option('-s --separator <char>', 'module separator', '/')
+	.helpOption('-h --help', 'display help for tape');
 
 program.parse();
