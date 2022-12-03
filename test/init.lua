15e1129b21ae6fe7ebf1d82eced91441a12f812b
safazi <safazi@airmail.cc>
2022-12-03 10:17:35 -0600

    test files

--- test/init.lua
+++ test/init.lua
@@ -1,5 +1,11 @@
 
 print('hello world')
 print(script, script.folder)
+
 script.Parent = workspace
-require(script.util).add(1,2)
+
+local add = require(script.util).add
+assert(add(1,2) == 3, 'add(1,2) == 3')
+
+local key = require('key.txt')
+assert(key == 'value', 'key.txt is not value')
